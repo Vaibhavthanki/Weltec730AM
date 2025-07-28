46d80ffdfc7401e64293557c6e7c2f6f0e6b5d07
Vaibhav Thanki <thankiv27@gmail.com>
2025-07-28 08:22:48 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,5 @@
+// console.log("Hello, World From External JS File!");
+// alert("Hello, World From External JS File!");
+
+console.log("Hello, World From Body tag!");
+alert("Hello, World From Body tag!");
